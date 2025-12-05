94b3052619c24351a27932fd4e08adbb3aa6e89b
tfeydev <tfeydeveloper@gmail.com>
2025-12-04 22:25:52 -0300

    add mastery-tracks

---
+++ src/app/data/mastery-track.data.ts
@@ -0,0 +1,37 @@
+export interface MasteryTrackItem {
+  name: string;
+  description: string;
+  iconPath: string;
+  repoUrl?: string;
+  category: 'Algorithms & Concurrency' | 'Database Mastery';
+}
+
+export const MASTERY_TRACK_DATA: MasteryTrackItem[] = [
+  {
+    name: 'Java DSA & Concurrency',
+    category: 'Algorithms & Concurrency',
+    description:
+      'Structured problem-solving practice covering algorithms, data structures, time/space optimization, threading, synchronization, and lock-based concurrency patterns.',
+    iconPath: 'assets/icons/java-logo.png',
+    repoUrl: 'https://github.com/techthordev/leetcode/tree/main/java',
+  },
+
+  {
+    name: 'PostgreSQL Mastery Track',
+    category: 'Database Mastery',
+    description:
+      'Advanced SQL practice including joins, window functions, indexing strategies, query optimization, normalization principles, and schema design.',
+    iconPath: 'assets/icons/postgresql-logo.png',
+    repoUrl: 'https://github.com/techthordev/leetcode/tree/main/postgresql',
+  },
+];
+
+export const getGroupedMasteryTracks = (data: MasteryTrackItem[]) => {
+  return data.reduce(
+    (acc, item) => {
+      (acc[item.category] = acc[item.category] || []).push(item);
+      return acc;
+    },
+    {} as Record<MasteryTrackItem['category'], MasteryTrackItem[]>,
+  );
+};
--- src/app/features/mastery-tracks/mastery-tracks.ts
+++ src/app/features/mastery-tracks/mastery-tracks.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MASTERY_TRACK_DATA, getGroupedMasteryTracks } from '../../data/mastery-track.data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mastery-tracks',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mastery-tracks.html',
   styleUrl: './mastery-tracks.css',
 })
 export class MasteryTracks {
-
+  tracks = MASTERY_TRACK_DATA;
+  groupedTracks = getGroupedMasteryTracks(this.tracks);
 }
